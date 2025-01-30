37c46a5892a0700f5c3514f5660d6787389d435a
Sadik Al Barid <70191239+R1B3n-13@users.noreply.github.com>
2025-01-30 18:50:09 +0000

    task: implement user entity (#20)

---
+++ src/main/java/com/devs/roamance/dto/response/BaseResponseDto.java
@@ -0,0 +1,17 @@
+package com.devs.roamance.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseResponseDto {
+
+    private int status;
+    private boolean success;
+    private String message;
+}
