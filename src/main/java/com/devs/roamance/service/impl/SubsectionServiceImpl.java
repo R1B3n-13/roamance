a6bb4f00f1b37a3a1c2557db723fd01c40ce9f0c
Yashrif Arifin <yaswoccho@gmail.com>
2025-03-30 12:23:00 +0600

    feat: add journalId to SubsectionCreateRequestDto and link subsections to journals in SubsectionServiceImpl

--- src/main/java/com/devs/roamance/service/impl/SubsectionServiceImpl.java
+++ src/main/java/com/devs/roamance/service/impl/SubsectionServiceImpl.java
@@ -21,13 +21,16 @@ import com.devs.roamance.dto.request.travel.journal.SubsectionUpdateRequestDto;
 import com.devs.roamance.dto.response.BaseResponseDto;
 import com.devs.roamance.dto.response.travel.journal.SubsectionListResponseDto;
 import com.devs.roamance.dto.response.travel.journal.SubsectionResponseDto;
+import com.devs.roamance.exception.JournalNotFoundException;
 import com.devs.roamance.exception.SubsectionNotFoundException;
 import com.devs.roamance.model.Location;
 import com.devs.roamance.model.travel.journal.ActivitySubsection;
+import com.devs.roamance.model.travel.journal.Journal;
 import com.devs.roamance.model.travel.journal.RouteSubsection;
 import com.devs.roamance.model.travel.journal.SightseeingSubsection;
 import com.devs.roamance.model.travel.journal.Subsection;
 import com.devs.roamance.model.travel.journal.SubsectionType;
+import com.devs.roamance.repository.JournalRepository;
 import com.devs.roamance.repository.SubsectionRepository;
 import com.devs.roamance.service.SubsectionService;
 
@@ -36,21 +39,37 @@ public class SubsectionServiceImpl implements SubsectionService {
   private static final Logger logger = LoggerFactory.getLogger(SubsectionServiceImpl.class);
 
   private final SubsectionRepository subsectionRepository;
+  private final JournalRepository journalRepository;
   private final ModelMapper modelMapper;
 
   public SubsectionServiceImpl(
       SubsectionRepository subsectionRepository,
+      JournalRepository journalRepository,
       ModelMapper modelMapper) {
     this.subsectionRepository = subsectionRepository;
+    this.journalRepository = journalRepository;
     this.modelMapper = modelMapper;
   }
 
   @Override
   public SubsectionResponseDto create(SubsectionCreateRequestDto requestDto) {
-    logger.info("Creating subsection with title: '{}'", requestDto.getTitle());
+    logger.info("Creating subsection with title: '{}' for journal with ID: {}",
+        requestDto.getTitle(), requestDto.getJournalId());
 
+    // Find the journal
+    Journal journal = journalRepository
+        .findById(requestDto.getJournalId())
+        .orElseThrow(() -> new JournalNotFoundException(
+            String.format(ResponseMessage.JOURNAL_NOT_FOUND, requestDto.getJournalId())));
+
+    // Create the subsection and link it to the journal
     Subsection subsection = mapToSubsectionType(requestDto);
+    subsection.setJournal(journal);
+
     Subsection savedSubsection = subsectionRepository.save(subsection);
+
+    logger.info("Successfully created subsection with ID: {} linked to journal: {}",
+        savedSubsection.getId(), journal.getTitle());
 
     return new SubsectionResponseDto(
         201, true, ResponseMessage.SUBSECTION_CREATE_SUCCESS, savedSubsection);
@@ -98,10 +117,23 @@ public class SubsectionServiceImpl implements SubsectionService {
 
   @Override
   public BaseResponseDto delete(UUID id) {
-    Subsection subsection = getById(id).getData();
+    logger.info("Deleting subsection with id: {}", id);
 
+    // Fetch subsection with its journal
+    Subsection subsection = getById(id).getData();
+    Journal journal = subsection.getJournal();
+
+    if (journal != null) {
+      logger.info("Detaching subsection from journal with ID: {}", journal.getId());
+      // Detach subsection from journal using the helper method in Journal
+      journal.removeSubsection(subsection);
+      subsection.setJournal(null);
+    }
+
+    // Delete the subsection
     subsectionRepository.delete(subsection);
 
+    logger.info("Successfully deleted subsection with id: {}", id);
     return new BaseResponseDto(200, true, ResponseMessage.SUBSECTION_DELETE_SUCCESS);
   }
 
