55a76ea822798ba2f8d8c4f582d909bee3c526ed
Yashrif Arifin <yaswoccho@gmail.com>
2025-04-14 13:04:27 +0600

    refactor: update ActivitySubsection fields to remove @NonNull annotations

--- src/main/java/com/devs/roamance/model/travel/journal/ActivitySubsection.java
+++ src/main/java/com/devs/roamance/model/travel/journal/ActivitySubsection.java
@@ -13,7 +13,8 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ActivitySubsection extends Subsection {
-  @Embedded @NonNull private Location location;
+  @Embedded
+  private Location location;
 
-  @NonNull private String activityName;
+  private String activityName;
 }
