463af15a970097d4a7836c8e0718dd53413ec2f7
Yashrif Arifin <yaswoccho@gmail.com>
2025-04-15 11:51:50 +0600

    fix: update request mapping for UserInfoController to include users prefix

--- src/main/java/com/devs/roamance/controller/UserInfoController.java
+++ src/main/java/com/devs/roamance/controller/UserInfoController.java
@@ -24,7 +24,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 @RestController
-@RequestMapping("/info")
+@RequestMapping("/users/info")
 public class UserInfoController {
 
     private final UserInfoService userInfoService;
