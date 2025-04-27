5f922fade5970db12ad04f20332ea10d286e8150
r1b3n <nebir2002@gmail.com>
2025-04-27 08:26:30 +0600

    feat: add itinerary creation with day plans & activities

---
+++ src/main/java/com/devs/roamance/util/ActivityUtil.java
@@ -0,0 +1,22 @@
+package com.devs.roamance.util;
+
+import com.devs.roamance.model.travel.itinerary.Activity;
+import com.devs.roamance.model.travel.itinerary.ActivityType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActivityUtil {
+
+  public void setActivityType(Activity activity, String inputType) {
+
+    // Manually set ActivityType
+    ActivityType activityType = ActivityType.fromString(inputType);
+    activity.setType(activityType);
+
+    // Set otherTypeName only for custom OTHER types
+    String otherTypeName =
+        (activityType == ActivityType.OTHER && inputType != null) ? inputType : null;
+
+    activity.setOtherTypeName(otherTypeName);
+  }
+}
---
+++ src/main/java/com/devs/roamance/util/ItineraryUtil.java
@@ -0,0 +1,86 @@
+package com.devs.roamance.util;
+
+import com.devs.roamance.dto.common.AiPoweredActivityDto;
+import com.devs.roamance.dto.common.AiPoweredDayPlanDto;
+import com.devs.roamance.dto.common.AiPoweredItineraryDto;
+import com.devs.roamance.model.travel.Location;
+import com.devs.roamance.model.travel.itinerary.Activity;
+import com.devs.roamance.model.travel.itinerary.DayPlan;
+import com.devs.roamance.model.travel.itinerary.Itinerary;
+import com.devs.roamance.model.user.User;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ItineraryUtil {
+
+  private final ActivityUtil activityUtil;
+  private final ModelMapper modelMapper;
+
+  public ItineraryUtil(ActivityUtil activityUtil, ModelMapper modelMapper) {
+    this.activityUtil = activityUtil;
+    this.modelMapper = modelMapper;
+  }
+
+  public void validateDayPlansAndActivities(Itinerary itinerary) {
+
+    for (DayPlan dayPlan : itinerary.getDayPlans()) {
+
+      itinerary.validateDayPlanDate(dayPlan);
+
+      for (Activity activity : dayPlan.getActivities()) {
+
+        dayPlan.validateNoTimeCollisions(activity);
+        activity.setId(null); // unset before persisting
+      }
+    }
+  }
+
+  public void mapToDayPlansAndActivities(
+      Itinerary itinerary, AiPoweredItineraryDto itineraryDto, User user) {
+
+    itinerary.setUser(user);
+    itinerary.setTitle(itineraryDto.getTitle());
+    itinerary.setDescription(itineraryDto.getDescription());
+    itinerary.setStartDate(itineraryDto.getStartDate());
+    itinerary.setEndDate(itineraryDto.getEndDate());
+    itinerary.setNotes(itineraryDto.getNotes());
+    itineraryDto
+        .getLocations()
+        .forEach(
+            location -> {
+              Location newLocation = modelMapper.map(location, Location.class);
+              itinerary.getLocations().add(newLocation);
+            });
+
+    List<DayPlan> dayPlans = new ArrayList<>();
+    for (AiPoweredDayPlanDto dayPlanDto : itineraryDto.getDayPlans()) {
+
+      DayPlan dayPlan = modelMapper.map(dayPlanDto, DayPlan.class);
+
+      dayPlan.setItinerary(itinerary);
+      dayPlan.setUser(user);
+
+      List<Activity> activities = new ArrayList<>();
+      for (AiPoweredActivityDto activityDto : dayPlanDto.getActivities()) {
+
+        Activity activity = modelMapper.map(activityDto, Activity.class);
+
+        activity.setId(UUID.randomUUID()); // this is temporarily set for validation
+        activity.setDayPlan(dayPlan);
+        activity.setUser(user);
+        activityUtil.setActivityType(activity, activityDto.getType());
+
+        activities.add(activity);
+      }
+
+      dayPlan.setActivities(activities);
+      dayPlans.add(dayPlan);
+    }
+
+    itinerary.setDayPlans(dayPlans);
+  }
+}
